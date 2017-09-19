d258bbf7de2954f4175ca293a3e778b8c0618aca
ramon <ramonhoeks@comaxx.nl>
2017-09-19 09:10:52 +0200

    Full screen mode added

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -69,4 +69,30 @@ progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => true);
 progress.addEventListener('mouseup', () => false);
 
-fullscreenButton.addEventListener('click', videoFullscreen);
+fullscreenButton.addEventListener('click', toggleFullScreen);
+
+
+
+var videoElement = document.getElementById("myvideo");
+
+function toggleFullScreen() {
+    if (!document.mozFullScreen && !document.webkitFullScreen) {
+        if (videoElement.mozRequestFullScreen) {
+            videoElement.mozRequestFullScreen();
+        } else {
+            videoElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else {
+            document.webkitCancelFullScreen();
+        }
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.keyCode == 13) {
+        toggleFullScreen();
+    }
+}, false);
